44fec76023fecac2ae941170e350735cb781ddeb
profadevairvitorio <profadevairvitorio@gmail.com>
2023-11-22 21:39:47 -0300

    chore: study about fuctions

---
+++ basic/functions/example_simple_fuction.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Definição de uma função que imprime "Hello, Go!"
+func saudacao() {
+	fmt.Println("Hello, Go!")
+}
+
+func main() {
+	// Chamada da função
+	saudacao()
+}
